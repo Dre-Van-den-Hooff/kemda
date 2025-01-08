0b172282646ce430f5d596387d2d0000697ae06d
Dré Van den Hooff <dre.vandenhooff@student.hogent.be>
2025-01-08 20:44:00 +0100

    Start api

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,6 +1,24 @@
+import type { StaticImageData } from 'next/image';
+
+type Project = {
+  id: number;
+  title: string;
+  shortDescription: string;
+  longDescription: string;
+  previewImage: StaticImageData | undefined;
+};
+
 export async function GET() {
-  const data = { message: 'Hello from server' };
-  return new Response(JSON.stringify(data), {
+  const projects: Project[] = [
+    {
+      id: 1,
+      title: 'Deuren',
+      shortDescription: '',
+      longDescription: '',
+      previewImage: undefined,
+    },
+  ];
+  return new Response(JSON.stringify(projects), {
     headers: { 'Content-Type': 'application/json' },
   });
 }
